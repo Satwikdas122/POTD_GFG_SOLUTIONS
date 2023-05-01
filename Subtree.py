70615e1877f6282f99792758524cf992cc8176ac
Satwik Das <109429663+Satwikdas122@users.noreply.github.com>
2023-05-01 23:19:39 +0530

    Create  Subtree.py

---
+++ Subtree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.map = {}
+
+    def duplicateSubtreeNaryTree(self, root: 'Node') -> int:
+        ans = 0
+        self.solve(root)
+
+        for e in self.map:
+            if self.map[e] > 1:
+                ans += 1
+
+        return ans
+
+    def solve(self, root: 'Node') -> str:
+        s = str(root.key)
+        for nei in root.children:
+            s += self.solve(nei)
+
+        self.map[s] = self.map.get(s, 0) + 1
+        return s
